13e55a408829156a8e3156a8864b0261d1059440
Ramalh <79301652+Ramalh@users.noreply.github.com>
2021-06-08 12:39:13 +0300

    Update Eukarya_bacteria.r

--- Eukarya_bacteria.r
+++ Eukarya_bacteria.r
@@ -9,8 +9,6 @@ asmbl <- read_delim("../All_asmbl.txt", "\t", escape_double = FALSE, col_names =
                                             "taxid" = "c",	"species_taxid" = "c",	"organism_name" = "c",	"gbrs_paired_asm" = "c",
                                             "ftp_path" = "c", "sname" = "c"), trim_ws = TRUE)
 
-
-asmbl <- read.delim("trimed_asmbl.txt", header = T)
 all_species <- root$X2 %in% asmbl$species_taxid
 species <- root[all_species,]
 
